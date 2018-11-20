2e4b26c2d8cc015b19c6e9c464ec39626aa8fa0d
Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2018-11-21 00:21:17 +0200

    Adding All

---
+++ APM/app/products/productListCtrl.js
@@ -0,0 +1,50 @@
+var app;
+(function (app) {
+    var productList;
+    (function (productList) {
+        var ProductListController = /** @class */ (function () {
+            function ProductListController() {
+                this.title = 'Products Title';
+                this.showImage = false;
+                this.products = [
+                    {
+                        "productId": 1,
+                        "productName": "Leaf Rake",
+                        "productCode": "GDN-0011",
+                        "releaseDate": new Date(2009, 2, 19),
+                        "description": "Leaf rake with 48-inch wooden handle.",
+                        "price": 19.95,
+                        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+                    },
+                    {
+                        "productId": 2,
+                        "productName": "Garden Cart",
+                        "productCode": "GDN-0023",
+                        "releaseDate": new Date(2010, 2, 18),
+                        "description": "15 gallon capacity rolling garden cart",
+                        "price": 32.99,
+                        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+                    },
+                    {
+                        "productId": 5,
+                        "productName": "Hammer",
+                        "productCode": "TBX-0048",
+                        "releaseDate": new Date(2013, 4, 21),
+                        "description": "Curved claw steel hammer",
+                        "price": 8.99,
+                        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+                    }
+                ];
+                var newProduct = new app.domain.Product(1, 'saw', 'X50', new Date(), 500, 'Show any description', 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png');
+                newProduct.price = newProduct.calculateDiscount(10);
+                this.products.push(newProduct);
+            }
+            ProductListController.prototype.toggleImage = function () {
+                this.showImage = !this.showImage;
+            };
+            return ProductListController;
+        }());
+        angular.module('productManagement')
+            .controller('ProductListCtrl', ProductListController);
+    })(productList = app.productList || (app.productList = {}));
+})(app || (app = {}));
