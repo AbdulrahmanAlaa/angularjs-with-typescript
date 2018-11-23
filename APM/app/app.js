9ef586a32fd64c42341e35c6afeaa9795004b793
Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2018-11-23 02:39:40 +0200

    adding routing

--- APM/app/app.js
+++ APM/app/app.js
@@ -1,4 +1,35 @@
 var app;
 (function (app) {
-    angular.module('productManagement', ['productResourceMock', 'common.services']);
+    var main = angular.module('productManagement', [
+        'ngRoute',
+        'productResourceMock',
+        'common.services'
+    ]);
+    main.config(routesConfig);
+    routesConfig.$inject = ['$routeProvider'];
+    function routesConfig($routeProvider) {
+        $routeProvider
+            .when('/productList', {
+            templateUrl: '/app/products/productListView.html',
+            controller: 'ProductListCtrl as vm'
+        })
+            .when('/productDetail/:productId', {
+            templateUrl: '/app/products/productDetailView.html',
+            controller: 'ProductDetailCtrl as vm'
+        })
+            .otherwise({ redirectTo: '/productList' });
+    }
+    main.run([
+        '$rootScope',
+        function ($rootScope) {
+            // see what's going on when the route tries to change
+            $rootScope.$on('$routeChangeStart', function (event, next, current) {
+                // next is an object that is the route that we are starting to go to
+                // current is an object that is the route where we are currently
+                var currentPath = current;
+                var nextPath = next;
+                console.log('Starting to leave  to go to', currentPath, nextPath);
+            });
+        }
+    ]);
 })(app || (app = {}));
--- APM/app/app.ts
+++ APM/app/app.ts
@@ -1,3 +1,25 @@
-module app{
-    angular.module('productManagement', ['productResourceMock','common.services']);
+module app {
+    var main = angular.module('productManagement',
+        [
+            'ngRoute',
+            'productResourceMock',
+            'common.services'
+        ]);
+
+    main.config(routesConfig);
+
+    routesConfig.$inject = ['$routeProvider'];
+    function routesConfig($routeProvider: ng.route.IRouteProvider) :void{
+        $routeProvider
+            .when('/productList', {
+                templateUrl: '/app/products/productListView.html',
+                controller: 'ProductListCtrl as vm'
+            })
+            .when('/productDetail/:productId', {
+                templateUrl: '/app/products/productDetailView.html',
+                controller: 'ProductDetailCtrl as vm'
+            })
+            .otherwise({ redirectTo:'/productList'});
+    }
+    
 }
